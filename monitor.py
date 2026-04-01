1da719fb963505262094db4fdc169b44ca31953e
Diego <diegoflogg11@gmail.com>
2026-04-01 13:28:00 -0500

    Refactoring

--- monitor.py
+++ monitor.py
@@ -4,32 +4,40 @@ import os
 from datetime import datetime
 import csv
 
+filename = "metrics.csv"
+file_exists = os.path.isfile(filename)
+
+def getMetrics():
+        cpu = psutil.cpu_percent(interval=None)
+        memory = psutil.virtual_memory().percent
+        disk = psutil.disk_usage("C:\\").percent
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        return cpu, memory, disk, current_time
+
+def printMetrics(cpu, memory, disk, current_time):
+        print(f"Recording... [Ctrl+C to Quit]")
+        print(f"Time: {current_time}")
+        print(f"CPU: {round(cpu)}%")
+        print(f"Memory: {round(memory)}%")
+        print(f"Disk : {round(disk)}%")
+
+def writeMetrics(writer, current_time, cpu, memory, disk):
+     writer.writerow([current_time, cpu, memory, disk])
+
 try:
-    with open("metrics.csv", "a", newline='') as f:
+    with open(filename, "a", newline='') as f:
         writer = csv.writer(f)
 
-        if f.tell() == 0:
-            writer.writerow(["Time", "CPU %", "Memory"])
+        if not file_exists:
+            writer.writerow(["Time", "CPU %", "Memory", "Disk %"])
 
         print("Recording metrics... Press Ctrl+C to stop.")
         
         while True:
-            now = datetime.now()
-            formatted_time_24hr = now.strftime("%Y-%m-%d %H:%M:%S")
-
-            cpu = psutil.cpu_percent(interval=None)
-            memory = psutil.virtual_memory().percent
-            disk = psutil.disk_usage("C:\\").percent
-
-            writer.writerow([formatted_time_24hr, cpu, memory])
+            
             f.flush()
-
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(f"Recording... [Ctrl+C to Quit]")
-            print(f"Time: {formatted_time_24hr}")
-            print(f"CPU: {round(cpu)}%")
-            print(f"Memory: {round(memory)}%")
-            print(f"Disk : {round(disk)}%")
             time.sleep(1)
 
 except KeyboardInterrupt:
